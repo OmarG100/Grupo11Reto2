024f55f5bd0c172c9811d46fc15cdf1bf783d01f
EduTeruya <eduardo.teruya.camt@gmail.com>
2023-01-15 13:00:09 -0500

    test

--- js/modules/header.js
+++ js/modules/header.js
@@ -3,14 +3,14 @@ import { fetchReadFoods, renderFoods } from "../utils/utils.js";
 const header = () => {
   const searchForm = document.getElementById('searchForm');
 
-  const searchCharacters = async (e) => {
+  const searchFoods = async (e) => {
     e.preventDefault();
     const name = searchForm.name.value;
-    const data = await fetchReadCharacters(1, name);
-    renderCharacters(data);
+    const data = await fetchReadFoods(1, name);
+    renderFoods(data);
   };
 
-  searchForm.addEventListener('submit', searchCharacters);
+  searchForm.addEventListener('submit', searchFoods);
 };
 
 export default header;
